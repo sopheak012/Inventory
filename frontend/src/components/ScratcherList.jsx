c21b040b8fdd449c4e370b5f3270dc2991948e79
sopheak012 <36690234+sopheak012@users.noreply.github.com>
2023-11-11 19:07:43 -0800

    test deploy v3

--- frontend/src/components/ScratcherList.jsx
+++ frontend/src/components/ScratcherList.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeScratcher, buyScratcher } from "../features/scratcherSlice";
 import styled from "styled-components";
